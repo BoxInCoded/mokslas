f906c71e55da4bc8e94e06d40fc4c57444429d72
BoxInCoded <matas.mts@icloud.com>
2021-04-07 17:35:41 +0300

    added custom JS

---
+++ Savarankiski_darbai/coming_soon/js/custom.js
@@ -0,0 +1,31 @@
+mybutton = document.getElementById("upBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  currentYOffset = self.pageYOffset;
+  initYOffset = currentYOffset;
+
+  var intervalId = setInterval(function () {
+    currentYOffset -= initYOffset * 0.05;
+    document.body.scrollTop = currentYOffset;
+    document.documentElement.scrollTop = currentYOffset;
+
+    if (self.pageYOffset == 0) {
+      clearInterval(intervalId);
+    }
+  }, 20);
+}
